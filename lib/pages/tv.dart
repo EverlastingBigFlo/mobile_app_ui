b7cba12c9f271241b16085d25d5afdf699bd9504
EverlastingLilflo <anjorin199@gmail.com>
2023-10-15 11:26:10 +0100

    changes

--- lib/pages/tv.dart
+++ lib/pages/tv.dart
@@ -16,7 +16,7 @@ class _tvState extends State<tv> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        elevation: 2,
+        elevation: 4,
         backgroundColor: Colors.white,
         centerTitle: true,
         title: Text(
@@ -24,99 +24,107 @@ class _tvState extends State<tv> {
           style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
         ),
       ),
+      backgroundColor: Colors.grey[100],
       body: Container(
         child: Column(
           children: [
 //first card here
-            Container(
-              color: Colors.white,
-              padding: EdgeInsets.all(18),
-              child: Column(
-                children: [
-                  Text(
-                    'Service Provider',
-                    style: TextStyle(
-                      fontSize: 17,
-                    ),
-                  ),
-                  Row(
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(12.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      DropdownButton<String>(
-                        value: dropdownValue,
-                        icon: Icon(Icons.arrow_drop_down_rounded),
-                        underline: Container(
-                          // Remove underline border
-                          decoration: BoxDecoration(
-                            border: Border(
-                              bottom: BorderSide(
-                                color: Colors.transparent,
+                      Text(
+                        'Service Provider',
+                        style: TextStyle(
+                            fontSize: 17, fontWeight: FontWeight.w500),
+                      ),
+                      Row(
+                        children: [
+                          DropdownButton<String>(
+                            value: dropdownValue,
+                            icon: Icon(Icons.keyboard_arrow_right_rounded),
+                            underline: Container(
+                              // Remove underline border
+                              decoration: BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                    color: Colors.transparent,
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
-                        ),
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            dropdownValue = newValue!;
-                          });
-                        },
-                        items: [
-                          DropdownMenuItem(
-                            value: 'DSTV',
-                            child: Row(
-                              children: [
-                                ClipOval(
-                                  child: Image(
-                                    image: AssetImage('lib/assets/op.png'),
-                                    fit: BoxFit.cover,
-                                    width: 40,
-                                    // height: 30,
-                                  ),
-                                ), // Use AssetImage within Image widget
-                                SizedBox(width: 5),
-                                Text('DSTV'),
-                              ],
-                            ),
-                          ),
-                          DropdownMenuItem(
-                            value: 'GOTV',
-                            child: Row(
-                              children: [
-                                ClipOval(
-                                  child: Image(
-                                    image: AssetImage('lib/assets/gotv.webp'),
-                                    fit: BoxFit.cover,
-                                    width: 40,
-                                    height: 40,
-                                  ),
-                                ), // Use AssetImage within Image widget
-                                SizedBox(width: 5),
-                                Text('GOTV'),
-                              ],
-                            ),
-                          ),
-                          DropdownMenuItem(
-                            value: 'Startimes',
-                            child: Row(
-                              children: [
-                                ClipOval(
-                                  child: Image(
-                                    image:
-                                        AssetImage('lib/assets/startimes.png'),
-                                    fit: BoxFit.cover,
-                                    width: 40,
-                                    height: 40,
-                                  ),
-                                ), // Use AssetImage within Image widget
-                                SizedBox(width: 5),
-                                Text('Startimes'),
-                              ],
-                            ),
-                          ),
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                dropdownValue = newValue!;
+                              });
+                            },
+                            items: [
+                              DropdownMenuItem(
+                                value: 'DSTV',
+                                child: Row(
+                                  children: [
+                                    ClipOval(
+                                      child: Image(
+                                        image: AssetImage('lib/assets/op.png'),
+                                        fit: BoxFit.cover,
+                                        width: 40,
+                                        // height: 30,
+                                      ),
+                                    ), // Use AssetImage within Image widget
+                                    SizedBox(width: 5),
+                                    Text('DSTV'),
+                                  ],
+                                ),
+                              ),
+                              DropdownMenuItem(
+                                value: 'GOTV',
+                                child: Row(
+                                  children: [
+                                    ClipOval(
+                                      child: Image(
+                                        image:
+                                            AssetImage('lib/assets/gotv.webp'),
+                                        fit: BoxFit.cover,
+                                        width: 40,
+                                        height: 40,
+                                      ),
+                                    ), // Use AssetImage within Image widget
+                                    SizedBox(width: 5),
+                                    Text('GOTV'),
+                                  ],
+                                ),
+                              ),
+                              DropdownMenuItem(
+                                value: 'Startimes',
+                                child: Row(
+                                  children: [
+                                    ClipOval(
+                                      child: Image(
+                                        image: AssetImage(
+                                            'lib/assets/startimes.png'),
+                                        fit: BoxFit.cover,
+                                        width: 40,
+                                        height: 40,
+                                      ),
+                                    ), // Use AssetImage within Image widget
+                                    SizedBox(width: 5),
+                                    Text('Startimes'),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          )
                         ],
                       )
                     ],
-                  )
-                ],
+                  ),
+                ),
               ),
             )
           ],
